bf0ed3a0936122ece98be1651e9ab38416752bbe
MaxXSoft <x@MaxXSoft.net>
2020-02-23 10:51:25 +0800

    (cast) added util 'cast'

---
+++ examples/cast.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+#include "cast.h"
+
+using namespace std;
+
+namespace {
+
+struct IntDouble {
+  int i;
+  double d;
+};
+
+struct FloatLong {
+  float f;
+  long l;
+};
+
+}  // namespace
+
+int main(int argc, const char *argv[]) {
+  IntDouble t = {42, 3.3};
+  auto bytes = xstl::IntPtrCast<8>(&t);
+  for (int i = 0; i < sizeof(IntDouble); ++i) {
+    cout << hex << static_cast<int>(bytes[i]) << ' ';
+  }
+  cout << endl;
+  auto fl = xstl::PtrCast<FloatLong>(&t);
+  cout << fl->f << ", " << fl->l << endl;
+  cout << hex << xstl::EndianCast(0x12345678u) << endl;
+  return 0;
+}
