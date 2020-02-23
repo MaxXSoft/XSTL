0caba8ecd94d7cfaecf5a2ac643bc530193a48da
MaxXSoft <x@MaxXSoft.net>
2020-02-23 08:45:08 +0800

    (src) modified some details

--- src/argh.h
+++ src/argh.h
@@ -7,56 +7,61 @@
 
 namespace xstl {
 
-using StrRef = const std::string &;
-
 class ArgumentHandler {
-public:
-    using HandFunc = std::function<int(StrRef)>;
+ public:
+  using HandFunc = std::function<int(const std::string &)>;
 
-    ArgumentHandler() {}
-    ~ArgumentHandler() {}
+  ArgumentHandler() {}
+  ~ArgumentHandler() {}
 
-    bool ParseArguments(int argc, const char *argv[]) {
-        auto ErrorHandler = [this](const std::string &str) {
-            if (error_handler_) error_handler_(str);
-            return false;
-        };
-        for (int i = 1; i < argc; ++i) {
-            if (argv[i][0] == '-') {
-                std::string arg_name(argv[i] + 1);
-                HandFunc handler;
-                if (argv[i][1] == '-') {
-                    auto sub = arg_name.substr(1);
-                    auto it = alias_dict_.find(sub);
-                    if (it == alias_dict_.end()) return ErrorHandler(sub);
-                    handler = it->second;
-                }
-                else {
-                    auto it = handlers_.find(arg_name);
-                    if (it == handlers_.end()) return ErrorHandler(arg_name);
-                    handler = it->second;
-                }
-                if (handler((i + 1 >= argc || argv[++i][0] == '-') ? "" : argv[i])) return false;
-            }
-            else {
-                auto it = handlers_.find("");
-                if (it == handlers_.end()) return ErrorHandler(argv[i]);
-                if (it->second(argv[i])) return false;
-            }
+  bool ParseArguments(int argc, const char *argv[]) {
+    auto ErrorHandler = [this](const std::string &str) {
+      if (error_handler_) error_handler_(str);
+      return false;
+    };
+    for (int i = 1; i < argc; ++i) {
+      if (argv[i][0] == '-') {
+        std::string arg_name(argv[i] + 1);
+        HandFunc handler;
+        if (argv[i][1] == '-') {
+          auto sub = arg_name.substr(1);
+          auto it = alias_dict_.find(sub);
+          if (it == alias_dict_.end()) return ErrorHandler(sub);
+          handler = it->second;
         }
-        return true;
+        else {
+          auto it = handlers_.find(arg_name);
+          if (it == handlers_.end()) return ErrorHandler(arg_name);
+          handler = it->second;
+        }
+        if (handler((i + 1 >= argc || argv[++i][0] == '-') ? "" : argv[i]))
+          return false;
+      }
+      else {
+        auto it = handlers_.find("");
+        if (it == handlers_.end()) return ErrorHandler(argv[i]);
+        if (it->second(argv[i])) return false;
+      }
     }
+    return true;
+  }
 
-    void AddHandler(const std::string &arg_name, HandFunc handler) { handlers_[arg_name] = handler; }
-    void SetErrorHandler(HandFunc error_handler) { error_handler_ = error_handler; }
-    void AddAlias(const std::string &alias, const std::string &source) { if (handlers_[source]) alias_dict_[alias] = handlers_[source]; }
+  void AddHandler(const std::string &arg_name, HandFunc handler) {
+    handlers_[arg_name] = handler;
+  }
+  void SetErrorHandler(HandFunc error_handler) {
+    error_handler_ = error_handler;
+  }
+  void AddAlias(const std::string &alias, const std::string &source) {
+    if (handlers_[source]) alias_dict_[alias] = handlers_[source];
+  }
 
-private:
-    std::map<std::string, HandFunc> handlers_;
-    HandFunc error_handler_;
-    std::map<std::string, HandFunc> alias_dict_;
+ private:
+  std::map<std::string, HandFunc> handlers_;
+  HandFunc error_handler_;
+  std::map<std::string, HandFunc> alias_dict_;
 };
 
-} // namespace xstl
+}  // namespace xstl
 
-#endif // XSTL_ARGH_H_
+#endif  // XSTL_ARGH_H_
--- src/raii.h
+++ src/raii.h
@@ -8,56 +8,68 @@
 
 namespace xstl {
 
-template<typename T>
+template <typename T>
 struct RemoveConstant {
-    using Type = typename std::conditional< std::is_const<T>::value, 
-                                            typename std::remove_const<T>::type, 
-                                            T >::type;
+  using Type =
+      typename std::conditional<std::is_const<T>::value,
+                                typename std::remove_const<T>::type,
+                                T>::type;
 };
 
 class RAII {
-public:
-    using FunctionType = std::function<void()>;
+ public:
+  using FunctionType = std::function<void()>;
 
-    explicit RAII(FunctionType construct, FunctionType destruct)
-            : destruct_(destruct), destruct_tag_(true) {
-            construct();
-    }
-    RAII(RAII &&rhs)   // move constructor
-            : destruct_(rhs.destruct_), destruct_tag_(rhs.destruct_tag_) {
-        rhs.destruct_tag_ = false;
-    }
-    RAII(const RAII &raii) = delete;   // disable copy constructor
-    ~RAII() { if (destruct_tag_) destruct_(); }
+  explicit RAII(FunctionType construct, FunctionType destruct)
+      : destruct_(destruct), destruct_tag_(true) {
+    construct();
+  }
+  RAII(RAII &&rhs)  // move constructor
+  : destruct_(rhs.destruct_), destruct_tag_(rhs.destruct_tag_) {
+    rhs.destruct_tag_ = false;
+  }
+  RAII(const RAII &raii) = delete;  // disable copy constructor
+  ~RAII() {
+    if (destruct_tag_) destruct_();
+  }
 
-    RAII& operator=(const RAII &raii) = delete;   // disable assignment operator
+  RAII &operator=(const RAII &raii) =
+      delete;  // disable assignment operator
 
-private:
-    FunctionType destruct_;
-    bool destruct_tag_;
+ private:
+  FunctionType destruct_;
+  bool destruct_tag_;
 };
 
 template <typename Res, typename Construct, typename Destruct>
 RAII MakeRAII(Res &res, Construct construct, Destruct destruct) {
-    // type check
-    static_assert(std::is_class<Res>::value, "Type Res is not a class or struct type.");
-    static_assert(std::is_member_function_pointer<Construct>::value, "Type Construct is not a member function.");
-    static_assert(std::is_member_function_pointer<Destruct>::value, "Type Destruct is not a member function.");
+  // type check
+  static_assert(std::is_class<Res>::value,
+                "Type Res is not a class or struct type.");
+  static_assert(std::is_member_function_pointer<Construct>::value,
+                "Type Construct is not a member function.");
+  static_assert(std::is_member_function_pointer<Destruct>::value,
+                "Type Destruct is not a member function.");
 
-    auto res_addr = std::addressof(const_cast<typename RemoveConstant<Res>::type &>(res));
-    return RAII(std::bind(construct, res_addr), std::bind(destruct, res_addr));
+  auto res_addr =
+      std::addressof(const_cast<typename RemoveConstant<Res>::type &>(res));
+  return RAII(std::bind(construct, res_addr),
+              std::bind(destruct, res_addr));
 }
 
 template <typename Res, typename Destruct>
 RAII MakeRAII(Res &res, Destruct destruct) {
-    // type check
-    static_assert(std::is_class<Res>::value, "Type Res is not a class or struct type.");
-    static_assert(std::is_member_function_pointer<Destruct>::value, "Type Destruct is not a member function.");
+  // type check
+  static_assert(std::is_class<Res>::value,
+                "Type Res is not a class or struct type.");
+  static_assert(std::is_member_function_pointer<Destruct>::value,
+                "Type Destruct is not a member function.");
 
-    auto res_addr = std::addressof(const_cast<typename RemoveConstant<Res>::Type &>(res));
-    return RAII([]() {}, std::bind(destruct, res_addr));
+  auto res_addr =
+      std::addressof(const_cast<typename RemoveConstant<Res>::Type &>(res));
+  return RAII([]() {}, std::bind(destruct, res_addr));
 }
 
-}
+}  // namespace xstl
 
-#endif // XSTL_RAII_H_
+#endif  // XSTL_RAII_H_
