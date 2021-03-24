58a63691cf429340ff05602edcd5652f85146066
MaxXSoft <x@MaxXSoft.net>
2021-03-24 14:55:23 +0800

    (range) fixed some faults

--- examples/deprecated/range.cpp
+++ examples/deprecated/range.cpp
@@ -6,22 +6,22 @@ using namespace std;
 
 int main(int argc, const char *argv[]) {
   cout << "range test 1:" << endl;
-  for (const auto &i : xstl::Range(10)) {
+  for (auto i : xstl::Range(10)) {
     cout << i << ' ';
   }
   cout << endl;
   cout << "range test 2:" << endl;
-  for (const auto &i : xstl::Range(10, 20)) {
+  for (auto i : xstl::Range(10, 20)) {
     cout << i << ' ';
   }
   cout << endl;
   cout << "range test 3:" << endl;
-  for (const auto &i : xstl::Range(10, 20, 2)) {
+  for (auto i : xstl::Range(10, 20, 2)) {
     cout << i << ' ';
   }
   cout << endl;
   cout << "range test 4:" << endl;
-  for (const auto &i :
+  for (auto i :
        xstl::Range<int>(1, [](int &v) { return (v *= 2) <= 10000; })) {
     cout << i << ' ';
   }
